d40b795102145ee0a964cf8526b42258d4e64408
U-barbara\Anders <Anders@barbara.(none)>
2009-07-20 14:07:34 +0200

    Started work on deployer. Small increment on handling artifact meta data.

---
+++ deployer/src/main/java/no/f12/agiledeploy/deployer/PackageSpecification.java
@@ -0,0 +1,42 @@
+package no.f12.agiledeploy.deployer;
+
+public class PackageSpecification {
+
+	private String groupId;
+	private String artifactId;
+	private String version;
+	private boolean snapshot;
+
+	public PackageSpecification(String groupId, String artifactId, String version, boolean snapshot) {
+		this.groupId = groupId;
+		this.artifactId = artifactId;
+		this.version = version;
+		this.snapshot = snapshot;
+	}
+
+	public String getArtifactPath() {
+		String result = this.groupId.replaceAll("\\.", "/") + "/" + this.artifactId + "/" + version;
+		return suffixIfSnapshot(result);
+	}
+
+	public Object getArtifactFileName() {
+		String fileName = artifactId + "-" + version;
+		return suffixIfSnapshot(fileName);
+	}
+
+	private String suffixIfSnapshot(String fileName) {
+		if (isSnapshot()) {
+			fileName += "-SNAPSHOT";
+		}
+		return fileName;
+	}
+
+	private boolean isSnapshot() {
+		return this.snapshot;
+	}
+
+	public String getMetadataFilename() {
+		return getArtifactPath() + "/maven-metadata.xml";
+	}
+
+}
---
+++ deployer/src/test/java/no/f12/agiledeploy/deployer/PackageSpecificationTest.java
@@ -0,0 +1,43 @@
+package no.f12.agiledeploy.deployer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PackageSpecificationTest {
+	
+	@Test
+	public void shouldResloveCorrectPathForArtifact() {
+		PackageSpecification spec = createDefaultSpect(false);
+		assertEquals("group/g/artifact/version", spec.getArtifactPath());
+	}
+
+	@Test
+	public void shouldResloveCorrectPathForArtifactIfSnapshot() {
+		PackageSpecification spec = createDefaultSpect(true);
+		assertEquals("group/g/artifact/version-SNAPSHOT", spec.getArtifactPath());
+	}
+
+	@Test
+	public void shouldResolveCorrectNameForFile() {
+		PackageSpecification spec = createDefaultSpect(false);
+		assertEquals("artifact-version", spec.getArtifactFileName());
+	}
+	
+	@Test
+	public void shouldResloveCorrectNameForFileWithSnapshot() {
+		PackageSpecification spec = createDefaultSpect(true);
+		assertEquals("artifact-version-SNAPSHOT", spec.getArtifactFileName());
+	}
+	
+	@Test
+	public void shouldResolveCorrectMetaDataFile() {
+		PackageSpecification spec = createDefaultSpect(false);
+		assertEquals("group/g/artifact/version/maven-metadata.xml", spec.getMetadataFilename());
+	}
+	
+	private PackageSpecification createDefaultSpect(boolean snapshot) {
+		PackageSpecification spec = new PackageSpecification("group.g", "artifact", "version", snapshot);
+		return spec;
+	}
+}
