18def21b64ec0f3275536e001a393d9b36a22858
Anders Sveen <anders@f12.no>
2009-08-17 19:00:14 +0200

    Cleaned up test

--- deployer/src/test/java/no/f12/agiledeploy/deployer/ConfigurationServiceTest.java
+++ deployer/src/test/java/no/f12/agiledeploy/deployer/ConfigurationServiceTest.java
@@ -1,17 +1,15 @@
 package no.f12.agiledeploy.deployer;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
@@ -19,71 +17,53 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class ConfigurationServiceTest {
 
+	private File workingDirectory;
+	private File environmentDirectory;
+	private File installationDirectory;
+
+	@Before
+	public void setupDirectories() throws IOException {
+		this.workingDirectory = TestDataProvider.getDefaultTempDir();
+		this.environmentDirectory = new File(workingDirectory, "test-artifact/test");
+		this.installationDirectory = new File(workingDirectory, "test-artifact/test/current");
+		createFiles(installationDirectory, workingDirectory);
+	}
+
 	@Test
 	public void shouldCopyFilesForCurrentEnvironmentIfTheyDontExist() throws IOException {
-		File installationDirectory = createFiles();
-
 		ConfigurationServiceImpl configService = createService();
-		configService.configure(installationDirectory, "test");
+		configService.configure(environmentDirectory, "test");
 
-		assertTrue(new File(installationDirectory, "datasource.properties").exists());
-	}
-
-	private ConfigurationServiceImpl createService() {
-		ConfigurationServiceImpl configService = new ConfigurationServiceImpl();
-		configService.setFileSystemAdapter(new FileSystemAdapterImpl());
-		return configService;
-	}
-
-	private File createFiles() throws IOException {
-		File baseArtifactDir = TestDataProvider.getDefaultArtifactDir();
-		File environmentDir = new File(baseArtifactDir, "test");
-		File installDir = new File(environmentDir, "current");
-
-		UnpackerService unpacker = new UnpackerServiceImpl();
-		unpacker.unpack(TestDataProvider.getZipFile(baseArtifactDir), installDir);
-
-		FileUtil.moveOneUp(new File(installDir, "myapp-server-0.1-SNAPSHOT"));
-
-		return environmentDir;
+		assertTrue(new File(workingDirectory, "test-artifact/test/datasource.properties").exists());
 	}
 
 	@Test
 	public void shouldNotCopyFilesIfTheyAlreadyExist() throws IOException {
-		File workingDirectory = createFiles();
-
-		File target = new File(workingDirectory, "datasource.properties");
-		FileWriter writer = new FileWriter(target);
-		writer.write("testing");
-		writer.close();
+		File target = new File(workingDirectory, "test-artifact/test/datasource.properties");
+		TestDataProvider.writeContentToFile(target, "testing");
 
 		ConfigurationServiceImpl configService = createService();
-		configService.configure(workingDirectory, "test");
+		configService.configure(environmentDirectory, "test");
 
-		FileReader reader = new FileReader(target);
-		BufferedReader bReader = new BufferedReader(reader);
-		assertEquals("testing", bReader.readLine());
-		bReader.close();
+		TestDataProvider.assertFileContains(target, "testing");
 	}
 
 	@Test
 	public void shouldCopyFilesForAllEnvironments() throws IOException {
-		File workingDirectory = createFiles();
 		ConfigurationServiceImpl configService = createService();
 
-		configService.configure(workingDirectory, "test");
+		configService.configure(environmentDirectory, "test");
 
-		assertTrue(new File(workingDirectory, "allenvs.properties").exists());
+		assertTrue(new File(workingDirectory, "test-artifact/test/allenvs.properties").exists());
 	}
 
 	@Test
 	public void shouldNotOverWriteCustomFileForEnvironmentWithFileForAllEnvironments() throws IOException {
-		File workingDirectory = createFiles();
 		ConfigurationServiceImpl configService = createService();
 
-		configService.configure(workingDirectory, "test");
+		configService.configure(environmentDirectory, "test");
 
-		Resource propertyFile = new DefaultResourceLoader().getResource("file:" + workingDirectory.getPath()
+		Resource propertyFile = new DefaultResourceLoader().getResource("file:" + environmentDirectory.getPath()
 				+ "/system.properties");
 		Properties props = PropertiesLoaderUtils.loadProperties(propertyFile);
 		assertTrue(props.get("env").equals("test"));
@@ -91,8 +71,6 @@ public class ConfigurationServiceTest {
 
 	@Test
 	public void shouldSymLinkToDataDirectoryIfItExists() throws IOException {
-		File environmentDirectory = createFiles();
-		File artifactDirectory = environmentDirectory.getParentFile();
 		createDataDir(environmentDirectory);
 
 		ConfigurationServiceImpl configService = new ConfigurationServiceImpl();
@@ -101,13 +79,27 @@ public class ConfigurationServiceTest {
 
 		configService.configure(environmentDirectory, "test");
 
-		verify(fsAdapter).createSymbolicLink(new File(artifactDirectory, "test/data"), new File(artifactDirectory, "test/current/data"));
+		verify(fsAdapter).createSymbolicLink(new File(workingDirectory, "test-artifact/test/data"),
+				new File(workingDirectory, "test-artifact/test/current/data"));
 	}
 
 	private File createDataDir(File baseDirectory) {
 		File dataDir = new File(baseDirectory, "data");
 		dataDir.mkdirs();
 		return dataDir;
+	}
+
+	private ConfigurationServiceImpl createService() {
+		ConfigurationServiceImpl configService = new ConfigurationServiceImpl();
+		configService.setFileSystemAdapter(new FileSystemAdapterImpl());
+		return configService;
+	}
+
+	private void createFiles(File unpackDir, File workingDir) throws IOException {
+		UnpackerService unpacker = new UnpackerServiceImpl();
+		unpacker.unpack(TestDataProvider.getZipFile(workingDir), unpackDir);
+
+		FileUtil.moveOneUp(new File(unpackDir, "myapp-server-0.1-SNAPSHOT"));
 	}
 
 	@After
--- deployer/src/test/java/no/f12/agiledeploy/deployer/TestDataProvider.java
+++ deployer/src/test/java/no/f12/agiledeploy/deployer/TestDataProvider.java
@@ -1,6 +1,12 @@
 package no.f12.agiledeploy.deployer;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.junit.Test;
@@ -26,16 +32,29 @@ public class TestDataProvider {
 		workingdirectory.mkdirs();
 		File result = new File(workingdirectory, zipFile.getName());
 		new FileSystemAdapterImpl().copyFile(zipFile, result);
-		
+
 		return result;
 	}
 
 	public static File getDefaultTempDir() {
 		return new File("./target/temp");
 	}
-	
+
 	public static File getDefaultArtifactDir() {
 		return new File(getDefaultTempDir(), "test-artifact");
+	}
+
+	public static void writeContentToFile(File target, String content) throws IOException {
+		FileWriter writer = new FileWriter(target);
+		writer.write(content);
+		writer.close();
+	}
+
+	public static void assertFileContains(File target, String content) throws FileNotFoundException, IOException {
+		FileReader reader = new FileReader(target);
+		BufferedReader bReader = new BufferedReader(reader);
+		assertEquals(content, bReader.readLine());
+		bReader.close();
 	}
 
 	@Test
